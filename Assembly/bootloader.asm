ec15d2f7b706e194b6bac6e592e14b0ab0cc6d7e
moraes <mooraesz123@gmail.com>
2023-05-12 12:13:41 -0300

    finish project

--- Assembly/bootloader.asm
+++ Assembly/bootloader.asm
@@ -1,5 +1,5 @@
 jmp short main
-nop ;preencher espaço
+nop 
 
 OEMName: 		db "MORAESOS"
 bytePerSector: 		dw 512
@@ -75,33 +75,32 @@ nop
 ;----------------------------------------------------------------------
 
 main:
-mov[driveNumber],dl
 
-mov ax,0x7c00  ;deixando predefinido o valor do reg de dados
-mov ds,ax
+mov [drvnum],dl
 
-mov ax,0x7c0 ;é passado 7c0 para quando ocorrer o bit shift resultar no valor correto
+mov ax, 0x7c00  
+mov ds, ax
+
+mov ax, 0x7c0 
 mov es, ax
 
-sub ax,0x200 ;definindo espaço para pilha
-mov sp,ax
-mov bp,ax
+sub ax, 0x200 
+mov sp, ax
+mov bp, ax
 
 ;----------------------------------------------------------------------
 ;13h  G   40x25  8x8   320x200  256/256K  1   A000  VGA,MCGA
 ;----------------------------------------------------------------------
 
-xor ax,ax
-mov al, 0x13  ; Definindo modo ded video
+xor ax, ax
+mov al, 0x13 
 int 0x10
-
 
 call whiteBack
 
 call addFrase
 
 call goToSecondStage
-
 
 jmp $
 
@@ -116,19 +115,25 @@ jmp $
 
 whiteBack:
 push bp
-mov bp,sp
+mov bp, sp
 pusha
 
-xor cx,cx ;horizontal
-mov dx,0x1c ;vertical em baixo da frase
-mov ah,0x0c
-mov al,0x0f
+xor cx, cx  ;horizontal
+mov dx, 0x1e  ;vertical em baixo da frase
+mov ah, 0x0c
+mov al, 0x0f
 restart:
 int 0x10
 inc cx
 cmp cx, 0x140
 je zerarHorizontal
 jmp restart
+
+fim:
+popa
+mov sp,bp
+pop bp
+ret
 
 zerarHorizontal:
 xor cx,cx
@@ -137,11 +142,6 @@ cmp dx, 0xc8
 je fim
 jmp restart
 
-fim:
-popa
-mov sp,bp
-pop bp
-ret
 
 ;----------------------------------------------------------------------
 ;AH    = 13h
@@ -156,16 +156,15 @@ ret
 
 addFrase:
 push bp
-mov bp,sp
+mov bp, sp
 pusha
 
-mov ah,0x13
-mov al,0x01
-xor bx,bx
-mov bh,0x00
-mov bl,0x0d
+mov ah, 0x13
+mov al, 0x01
+xor bx, bx
+mov bl, 0x0f
 mov cx, 0x10
-mov dh,0x10
+mov dh,0x1c
 mov dl,0x04
 mov bp,mensagem
 int 0x10 
@@ -188,15 +187,24 @@ ret
 ;----------------------------------------------------------------------
 
 goToSecondStage:
-mov ah,0x02
+mov ah, 2
 mov al, 3 ;number of sectors to read
 mov ch, 0 ;Cylinder number
 mov cl, 2 ;starting sector number,2 because 1 was already loaded in memory
 mov dh, 0 ;head number
-mov dl, [driveNumber]
+mov dl, [drvnum]
 mov bx, stage2
 int 0x13
 jmp stage2
+
+
+drvnum: db 0
+
+;Variaveis
+mensagem: db 'Me contrata! \o/'
+
+times(510 - ($ - $$)) db 0x00
+dw 0xAA55
 
 ;----------------------------------------------------------------------
 ;------------------------Stage1 Finish!--------------------------------
@@ -223,46 +231,61 @@ jmp $
 ;----------------------------------------------------------------------
 paint:
 push bp
-mov bp,sp
+mov bp, sp
 pusha
 
 xor ax,ax
 mov ds,ax
 mov si,ax
 
-push 0x0f ; color -> first == [sp + 6]  
-push 0x00 ; X value -> second == [sp + 0x04]
-push 0xc7 ; Y value -> third == [sp + 0x02]
-push 0x00 ; reps -> fourth == [sp]
+push 0x0f ;color -> first == [sp + 6]  
+push 0x00 ;X value -> second == [sp + 0x04]
+push 0xc7 ;Y value -> third == [sp + 0x02]
+push 0x00 ;reps -> fourth == [sp]
 
 mov di,sp ;em 16bits,não é possivel fazer algumas operaçoes com sp e por isso usaremos outro registrador para guardar dados
-mov bl, byte [0x7c00 + dados + si] ;repetiçoes	
+mov bl, byte [0x7c00 + dados + di] ;repetiçoes	
 
 inicio:
 mov dx,[di + 0x02] ;Posiçao Vertical
 
 backOne:
 xor bh,bh
-mov dx,[di + 0x04]
+mov cx,[di + 0x04]
 mov al, [di + 0x06]
 mov ah, 0x0c
 int 0x10
-dec bl ; Diminui uma repetiçao
+dec bl ;Diminui uma repetiçao
 mov [di],bl ;salvando reps que falta 
+
 cmp bl,0
 je changeColor
+
 volta2:
 xor ax,ax 
-mov ax, word[di + 0x04]
+mov ax, word [di + 0x04]
 inc ax
 cmp ax, 0x13f ;319
 je prepararRetorno
-mov [di + 0x04],ax
+mov [di + 0x04] , ax
 jmp backOne
 
 
+prepararRetorno:
+xor ax,ax
+mov [di + 0x04], ax ; X
+mov ax, [di + 0x02] ; Y
+dec ax
+mov [di + 0x02], ax
+inc si
+mov bl, byte [0x7c00 + dados + si]
+cmp bl, 0xff
+je final
+jmp inicio
+
+
 changeColor:
-mov al,[di + 0x06]
+mov al, [di + 0x06]
 cmp al, 0x0f
 je preto
 mov al,0x0f
@@ -270,37 +293,24 @@ jmp salvar
 
 
 preto: 
-mov al,0x00
-jmp salvar
-
+mov al, 0x00
+;jmp salvar
 
 salvar:
 mov [di + 0x06],al
 inc si
+
 mov bl, byte [0x7c00 + dados + si]
-cmp bl,0xff
-je fim
-mov [di],bl
-jmp volta2
-
-
-prepararRetorno:
-xor ax,ax
-mov [di + 0x04],ax ; X
-mov ax, [di + 0x02] ; Y
-dec ax
-mov [di + 0x02], ax
-inc si
-mov bl, byte[0x7c00 + dados + si]
 cmp bl, 0xff
-je fim
-jmp inicio
-
+je final
+mov [di], bl
+jmp volta2
 
 popa
 mov sp,bp
 pop bp
 ret
+
 
 dados: 	db 109,62,149,109,62,149,109,62,149,109,62,149,109,62,149,82,83,6,23,126,82,83,6,23,126,82,83,6,23,126,82,83,6
 	db 23,126,82,83,6,23,126,63,102,6,23,126,63,102,6,23,126,63,102,6,23,126,63,102,6,23,126,63,102,6,23,126,54,43,6
@@ -329,8 +339,3 @@ dados: 	db 109,62,149,109,62,149,109,62,149,109,62,149,109,62,149,82,83,6,23,126
 	db 28,17,4,23,111,69,34,17,17,21,46,116,69,34,17,17,21,46,116,69,34,17,17,21,46,116,69,34,17,17,21,46,116,69,34,17,17,21,46,116,74,124,122
 	db 74,124,122,74,124,122,74,124,122,74,124,122,82,112,126,82,112,126,82,112,126,82,112,126,82,112,126,87,99,134,87,99,134,87,99,134,87,99
 	db 134,87,99,134,87,99,134,97,79,144,97,79,144,97,79,144,97,79,144,97,79,144,115,43,162,115,43,162,115,43,162,115,43,255
-
-;Variaveis
-mensagem: db 'Me contrata! \o/'
-
-dw 0xAA55
